ea8177f4b1f4306b85ea429ebd8afb5c0404fdc5
Georgi <Georgi@Georgi-PC>
2016-02-16 12:50:58 +0200

    za pregled ot Sasho

--- main.c
+++ main.c
@@ -1,24 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.c
- * Author: Georgi
- *
- * Created on 16 Февруари 2016, 10:47
- */
-
 #include <stdio.h>
-#include <stdlib.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+#define IN	1
+#define OUT	0
+#define MAXLENGTH	10
 
-    return (EXIT_SUCCESS);
+/* print a histogram of the lengths of words in the input */
+//
+int main()
+{
+	int c, i, j, length, state;
+	int lengths[MAXLENGTH];
+	int nlong;
+	/* lengths[0] is the number of words of length 1. nlong is the number
+	   of words whose length is greater than MAXLENGTH. */
+
+	for (i = 0; i < MAXLENGTH; i++)
+		lengths[i] = 0;
+	nlong = 0;
+
+	state = OUT;
+	length = 0;
+	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\n' || c == '\t') {
+			if (state == IN) {
+				if (length < MAXLENGTH)
+					++lengths[length - 1]; // what is the value of lenght here? isnt't it zero
+				else
+					++nlong;
+			}
+			length = 0;
+		} else {
+			state = IN;
+			++length;
+		}
+	}
+
+	for (i = 0; i < MAXLENGTH; i++) {
+		printf(" %2d: ", i + 1);
+		for (j = 0; j < lengths[i]; j++)
+			putchar('#');
+		putchar('\n');
+	}
+	printf(">%2d: ", MAXLENGTH);
+	for (j = 0; j < nlong; j++)
+		putchar('#');
+	putchar('\n');
+        return 0;
 }
-
